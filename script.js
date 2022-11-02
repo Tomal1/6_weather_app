a45df2df3796cd9bddc8df0bcfff4fd25cf52166
Minhazul Islam <minhazulislam@hotmail.com>
2022-11-02 11:03:38 +0000

    added a event listner which updates location on screen with input value

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let cityInput = document.querySelector("#cityInput");
 let searchBtn = document.querySelector("#searchBtn");
 let cityName = document.querySelector("#cityName");
 
+searchBtn.addEventListener("click", function(event){
+    event.preventDefault() // our function is inside a form so we have to stop it from submitting
 
- 
+    cityName.innerHTML = "--"+cityInput.value+"--"
 
 
 
-
+})
 
 
 
