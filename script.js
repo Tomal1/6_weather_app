ca6b7f3cdd751edc49bb27eeaa2337801cbdb1ae
Minhazul Islam <minhazulislam@hotmail.com>
2022-11-02 11:07:11 +0000

    fetch reqest added which takes URL with location/city name

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ let cityName = document.querySelector("#cityName");
 
 searchBtn.addEventListener("click", function(event){
     event.preventDefault() // our function is inside a form so we have to stop it from submitting
-
     cityName.innerHTML = "--"+cityInput.value+"--"
+
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+cityInput.value+"&appid=58fa8407237375f8467842ce20027a4c")
+    .then(function (response){
+        response.json()
+        console.log(response);
+
+    })
 
 
 
