89e50a883f5ca3dffc245d78bd23efa19b96a645
Minhazul Islam <minhazulislam@hotmail.com>
2022-11-02 18:17:55 +0000

    5 cards added on html and spaced evenly

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let cityInput = document.querySelector("#cityInput");
 let searchBtn = document.querySelector("#searchBtn");
 let cityName = document.querySelector("#cityName");
+let display = document.querySelector("#card");
 
 searchBtn.addEventListener("click", function(event){
     event.preventDefault() // our function is inside a form so we have to stop it from submitting
@@ -10,11 +11,7 @@ searchBtn.addEventListener("click", function(event){
     .then(function (response){
         response.json()
         console.log(response);
-
     })
-
-
-
 })
 
 
