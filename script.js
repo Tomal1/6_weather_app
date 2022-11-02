3374a070197e1a53286a8c73aec220e727181958
Minhazul Islam <minhazulislam@hotmail.com>
2022-11-02 20:43:45 +0000

    added 5 span tags to first card with different class names and populated the first card with weather data

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 let cityInput = document.querySelector("#cityInput");
 let searchBtn = document.querySelector("#searchBtn");
 let cityName = document.querySelector("#cityName");
-let display = document.querySelector("#card");
+let card = document.querySelector("#card");
+let dispContainer = document.querySelector("#dispContainer");
+
 
 searchBtn.addEventListener("click", function(event){
-    event.preventDefault() // our function is inside a form so we have to stop it from submitting
-    cityName.innerHTML = "--"+cityInput.value+"--"
+    event.preventDefault(); // our function is inside a form so we have to stop it from submitting
 
+//    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=52.4492074&lon=-2.0514646&appid=58fa8407237375f8467842ce20027a4c")
     fetch("https://api.openweathermap.org/data/2.5/forecast?q="+cityInput.value+"&appid=58fa8407237375f8467842ce20027a4c")
     .then(function (response){
-        response.json()
-        console.log(response);
+        return response.json();
     })
+    .then(function (data) {
+        //Using console.log to examine the data
+        console.log(data);
+
+        cityName.textContent = "--"+data.city.name+"--";
+
+        
+        document.querySelector(".tempature").textContent = "tempature: " + data.list[0].main.temp;
+        document.querySelector(".date").textContent = "date: " + data.list[0].dt_txt;
+        document.querySelector(".humidity").textContent = "humidity: " + data.list[0].main.humidity;
+        document.querySelector(".wind").textContent = "wind speed: "+ data.list[0].wind.speed + " mph";
+        
+
+  
+          //Appending the dynamically generated html to the div associated with the id="users"
+          //Append will attach the element as the bottom most child.
+        //   usersContainer.append(userName);
+        //   usersContainer.append(userUrl);
+       
+    });
 })
-
-
-
-
-
-
-
-
